de771f18c7e8a99ad50a67af6a90f94074968078
Paul Soderberg <pdsoderb@gmail.com>
2023-05-24 12:19:40 -0700

    Added to project section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,4 +1,7 @@
+import classes from './Projects.module.css';
+
 import Section from '../layout/Section';
+import Button from '../ui/Button';
 
 const Projects = () => {
   return (
@@ -7,7 +10,19 @@ const Projects = () => {
       description='Here you will find some of the personal and clients projects that I created with each project containing its own case study'
       id='projects'
     >
-      This is a child
+      <div className='project flex flex-col md:grid grid-cols-2 gap-10 items-center'>
+        <div className='project__img'>
+          <img className={classes['img-todo']} />
+        </div>
+        <div className='project__desc flex flex-col items-center md:items-start'>
+          <h3>Todo</h3>
+          <p className='py-6'>
+            Todo is a simple todo application to keep track of your weekly tasks. It's a complete full-stack application
+            with authentication and a database.
+          </p>
+          <Button title='Tech' type='button' />
+        </div>
+      </div>
     </Section>
   );
 };
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps
 const Button: FC<ButtonProps> = (props) => {
   return (
     <button
-      className='bg-yellow-300 uppercase px-10 py-4 rounded-md tracking-wider font-semibold text-lg hover:scale-105 transition-transform shadow-md'
+      className='bg-yellow-300 uppercase px-10 py-3 rounded-md tracking-wider font-semibold text-lg hover:scale-105 transition-transform shadow-md'
       {...props}
     >
       {props.title}
