3cf2f3319a5d646b0c87da8583f2e96e325ffe4c
Sody15 <pdsoderb@gmail.com>
2023-05-24 17:06:33 -0700

    style changes

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
       description='Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology'
       id='about'
     >
-      <div className='md:grid grid-cols-2 gap-6'>
+      <div className='md:grid grid-cols-2 gap-6 text-center md:text-left'>
         <div className='get-to-know'>
           <h3 className='pb-8'>Get to know me!</h3>
           <p>
@@ -16,6 +16,7 @@ const About = () => {
             perspiciatis veniam necessitatibus impedit fuga. Atque mollitia placeat in asperiores neque ab fugit
             voluptas, eaque deleniti vel.
           </p>
+          <br />
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias nisi beatae molestiae consequatur ducimus
             architecto quaerat omnis inventore ea delectus? Vero excepturi sint eum minima animi facilis, aliquam nihil
@@ -24,7 +25,7 @@ const About = () => {
         </div>
         <div className='skills pt-20 md:pt-0'>
           <h3 className='pb-8'>My Skills</h3>
-          <div className='flex flex-wrap gap-3'>
+          <div className='flex flex-wrap gap-3 justify-center md:justify-start'>
             {mySkills.map((skill) => (
               <Tag title={skill} key={skill} />
             ))}
