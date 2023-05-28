65da71e52d6ed733dc1b30d3e3597e40f2a224ee
Sody15 <pdsoderb@gmail.com>
2023-05-27 19:12:43 -0700

    Added contact form animations

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,24 +1,35 @@
 import classes from './Contact.module.css';
 
 import Section from '../layout/Section';
-import { FormEvent, useRef } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { motion } from 'framer-motion';
+
+import { BsEmojiSmile } from 'react-icons/bs';
 
 const Contact = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const form = useRef<HTMLFormElement>(null);
 
-  const submitForm = (e: FormEvent<HTMLFormElement>) => {
+  const submitForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('called');
+    setIsSubmitting(true);
 
-    emailjs.sendForm('service_2yto9gl', 'template_9tpabmi', form.current, 'flI8Hu3OYVqrDYBUl').then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
+    try {
+      await emailjs.sendForm(
+        'service_2yto9gl',
+        'template_9tpabmi',
+        form.current as HTMLFormElement,
+        'flI8Hu3OYVqrDYBUl'
+      );
+    } catch (err) {
+      console.log(err);
+    }
+
+    // const timer = new Promise((resolve) => setTimeout(resolve, 2000));
+    // await timer;
+
+    setIsSubmitting(false);
   };
 
   return (
@@ -29,6 +40,59 @@ const Contact = () => {
       className='bg-slate-200 max-w-full'
     >
       <form className={classes.form} onSubmit={submitForm} ref={form}>
+        {/* Submit Background Animation */}
+        <motion.div
+          className='form-bg w-6 h-6 bg-primary absolute bottom-0 right-0 rounded-full z-30 hidden'
+          initial={false}
+          animate={isSubmitting ? { scale: 100, display: 'block' } : { scale: 0 }}
+          transition={{
+            duration: 0.5,
+            ease: 'easeIn',
+          }}
+        ></motion.div>
+
+        {/* Submit Text Animation */}
+        <motion.div
+          className='w-full h-full bg-primary absolute top-0 left-0  z-30 overflow-hidden justify-center items-center text-white hidden'
+          animate={isSubmitting ? { opacity: 1, display: 'flex' } : { opacity: 0, display: 'none' }}
+          transition={{
+            delay: 0.5,
+            duration: 0.5,
+            ease: 'easeIn',
+          }}
+        >
+          <div className='flex flex-col items-center gap-5'>
+            <span className='flex gap-3 items-center'>
+              <h3>Thanks!</h3>
+              <motion.h3
+                initial={{ opacity: 0, translateY: '-50%' }}
+                animate={!isSubmitting ? { opacity: 0, translateY: -100 } : { opacity: 100, translateY: 0 }}
+                transition={{
+                  delay: 1,
+                  duration: 0.5,
+                  ease: 'backInOut',
+                }}
+                className='text-4xl'
+              >
+                <BsEmojiSmile />
+              </motion.h3>
+            </span>
+
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={!isSubmitting ? { opacity: 0 } : { opacity: 1 }}
+              transition={{
+                delay: 1.5,
+                duration: 0.5,
+                ease: 'easeIn',
+              }}
+            >
+              I'll be in touch soon!
+            </motion.p>
+          </div>
+        </motion.div>
+
+        {/* Form Fields */}
         <div>
           <label className={classes.label} htmlFor='user_name'>
             Name
