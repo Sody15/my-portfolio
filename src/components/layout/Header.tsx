f52316d07fb829e1ed8d9ef4312268b9419b2dd5
Paul Soderberg <pdsoderb@gmail.com>
2023-05-30 16:21:04 -0700

    Added my face :)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 
 import useClickOutside from '../../hooks/useClickOutside';
 import { scrollIntoView } from '../../utils/scrollTo';
+import faceUrl from '../../assets/face.png';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { IoMdClose } from 'react-icons/io';
@@ -29,7 +30,9 @@ const Header = () => {
     <header className='fixed uppercase font-semibold z-50'>
       <div className='text-lg fixed flex justify-between z-50 px-5 lg:px-12 py-3 bg-white md:shadow-sm top-0 max-w-[120rem] w-full whitespace-nowrap'>
         <div className='header__logo flex items-center gap-4'>
-          <span className='header__logo-icon h-12 w-12 bg-primary inline-block rounded-full'></span>
+          <span className='header__logo-icon h-14 w-14 bg-primary rounded-full flex justify-center items-center'>
+            <img className='w-8' src={faceUrl} alt='my face' />
+          </span>
           <span className='header__logo-name'>Paul Soderberg</span>
         </div>
 
