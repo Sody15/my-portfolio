2924d1d97ef1d2c9b884dd1b281c2cf3a3a222b5
Paul Soderberg <pdsoderb@gmail.com>
2023-07-11 14:12:36 -0700

    Another fix for tech images

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Tag: FC<{ title: string; imgSrc?: string }> = ({ title, imgSrc }) => {
   return (
     <span className='tag py-3 px-4 bg-slate-200 text-gray-600 rounded-md font-semibold flex flex-col gap-2 items-center justify-center w-28 text-center leading-4'>
-      {imgSrc && <img src={'src/assets/tech-logos/' + imgSrc} className='w-10'></img>}
+      {imgSrc && <img src={'./tech-logos/' + imgSrc} className='w-10'></img>}
       {title}
     </span>
   );
