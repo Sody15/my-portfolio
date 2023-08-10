41713451823b74653e1fbf2df627470147f80cc7
Sody15 <pdsoderb@gmail.com>
2023-08-10 11:48:32 -0700

    added alt prop to tag imgs

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Tag: FC<{ title: string; imgSrc?: string }> = ({ title, imgSrc }) => {
   return (
     <span className='tag py-3 px-4 bg-slate-200 text-gray-600 rounded-md font-semibold flex flex-col gap-2 items-center justify-center w-24 text-center leading-4'>
-      {imgSrc && <img src={'./tech-logos/' + imgSrc} className='w-10'></img>}
+      {imgSrc && <img src={'./tech-logos/' + imgSrc} className='w-10' alt={'tag-' + title.toLowerCase()}></img>}
       {title}
     </span>
   );
